3c0b41241cc88a71d3e36d7f1c15afb9484deb7c
mdaurbekov <dreamkov@gmail.com>
2022-07-20 18:55:25 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = new int[]{1, 2, 3, 4};
+
+        int max = new SalesManager(array).max();
+        System.out.println(max);
+
+    }
+}
